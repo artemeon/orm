c6dbe750c697d27c4563a7772ec9522cf4906442
Christoph Kappestein <christoph.kappestein@artemeon.de>
2023-03-03 16:03:13 +0100

    update

---
+++ src/EntityManager.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Artemeon\Orm;
+
+use Artemeon\Database\ConnectionInterface;
+use Artemeon\Orm\Condition\OrmConditionInterface;
+
+class EntityManager
+{
+    private ConnectionInterface $connection;
+    private QueryBuilder $queryBuilder;
+    private FieldMapper $fieldMapper;
+
+    public function __construct(ConnectionInterface $connection, QueryBuilder $queryBuilder, FieldMapper $fieldMapper)
+    {
+        $this->connection = $connection;
+        $this->queryBuilder = $queryBuilder;
+        $this->fieldMapper = $fieldMapper;
+    }
+
+    /**
+     * @param array<OrmConditionInterface> $conditions
+     * @param array<OrmOrderBy> $sorting
+     * @return array<EntityInterface>
+     * @throws Exception\OrmException
+     */
+    public function findAll(string $targetClass, array $conditions = [], array $sorting = []): array
+    {
+        [$query, $params] = $this->getQuery($targetClass, $conditions, $sorting);
+
+        $entities = [];
+        $result = $this->connection->fetchAllAssociative($query, $params);
+        foreach ($result as $row) {
+            $entity = new $targetClass();
+            $this->fieldMapper->map($entity, $row);
+            $entities[] = $entity;
+        }
+
+        return $entities;
+    }
+
+    /**
+     * @param array<OrmConditionInterface> $conditions
+     * @param array<OrmOrderBy> $sorting
+     * @throws Exception\OrmException
+     */
+    public function findOne(string $targetClass, array $conditions = [], array $sorting = []): ?EntityInterface
+    {
+        [$query, $params] = $this->getQuery($targetClass, $conditions, $sorting);
+
+        $row = $this->connection->fetchAssociative($query, $params);
+        if (empty($row)) {
+            return null;
+        }
+
+        $entity = new $targetClass();
+        $this->fieldMapper->map($entity, $row);
+        return $entity;
+    }
+
+    /**
+     * @param array<OrmConditionInterface> $conditions
+     * @throws Exception\OrmException
+     */
+    public function getCount(string $targetClass, array $conditions): int
+    {
+        $from = $this->queryBuilder->buildFrom($targetClass);
+        $query = 'SELECT COUNT(*) AS cnt ' . $from . ' WHERE 1=1 ';
+
+        $params = [];
+        foreach ($conditions as $condition) {
+            $query.= $condition->getStrWhere() . ' ';
+            $params = array_merge($params, $condition->getArrParams());
+        }
+
+        $row = $this->connection->fetchOne($query, $params);
+        if (!isset($row['cnt'])) {
+            return 0;
+        }
+
+        return (int) $row['cnt'];
+    }
+
+    private function getQuery(string $targetClass, array $conditions = [], array $sorting = []): array
+    {
+        $from = $this->queryBuilder->buildFrom($targetClass);
+        $query = 'SELECT * ' . $from . ' WHERE 1=1 ';
+
+        $params = [];
+        foreach ($conditions as $condition) {
+            $query.= $condition->getStrWhere() . ' ';
+            $params = array_merge($params, $condition->getArrParams());
+        }
+
+        if (count($sorting) > 0) {
+            $query.= ' ORDER BY ';
+            foreach ($sorting as $sort) {
+                $query.= $sort->getStrOrderBy() . ' ';
+            }
+        }
+
+        return [$query, $params];
+    }
+}
---
+++ src/OrmOrderBy.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Artemeon\Orm;
+
+/**
+ * A single order-by statement.
+ * Pass them to the objectlist-instance before loading the resultset.
+ * Pass values using the syntax "columnmame ORDER". Don't add "ORDER BY" or commas since this
+ * will be done by the mapper.
+ */
+class OrmOrderBy
+{
+    private string $orderBy;
+
+    public function __construct(string $orderBy)
+    {
+        $this->orderBy = " ".$orderBy." ";
+    }
+
+    public function setOrderBy(string $orderBy): void
+    {
+        $this->orderBy = $orderBy;
+    }
+
+    public function getOrderBy(): string
+    {
+        return $this->orderBy;
+    }
+}
